60e60e5ebc1fb767e241e307bd3fea1055a30803
AlexAcevedo447 <paniaguaalexis40@gmail.com>
2023-07-14 21:36:40 -0500

    Initial commit

--- src/db/database.provider.ts
+++ src/db/database.provider.ts
@@ -7,6 +7,6 @@ export const databaseProviders = [
     {
         provide: 'DATABASE_CONNECTION',
         useFactory: (): Promise<typeof mongoose> =>
-            mongoose.connect('mongodb://localhost:27017'),
+            mongoose.connect(<string>process.env.DB_URL),
     },
 ];
